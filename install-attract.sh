5e4c5074d9cc73b7a67ba6b9d93f1d87ca079931
PiDiaries <iainjohnston79@gmail.com>
2016-05-16 23:01:54 +0100

    Update install-attract.sh

--- install-attract.sh
+++ install-attract.sh
@@ -13,13 +13,13 @@ source "/home/osmc/RetroPie/scripts/retrosmc-config.cfg"
 
 # setting up the menu
 
-cmd=(dialog --backtitle "attractmode scripts installation - Version $CURRENT_VERSION" --menu "Welcome to the attractmode installation.\nWhat would you like to do?\n " 14 50 16)
+cmd=(dialog --backtitle "attract-mode scripts installation - Version $CURRENT_VERSION" --menu "Welcome to the Attract-Mode Installation.\nWhat would you like to do?\n " 14 50 16)
 
-options=(1 "Install The Attract Scripts"
+options=(1 "Install  Retrosmc + Attract-mode"
          2 "Install Launcher Addon"
          3 "Remove Launcher Addon"
          4 "Update scripts"
-         5 "compile Attract_Mode *if not already done do this first*")
+         )
          
 choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
 
@@ -27,13 +27,84 @@ for choice in $choices
 do
     case $choice in
         1)
+# create the config directory and file
+
+            mkdir -p /home/osmc/RetroPie/scripts
+            touch "/home/osmc/RetroPie/scripts/retrosmc-config.cfg"
+
+# install some programs needed to run the installation and retrosmc
+
+            sudo apt-get update 2>&1 | dialog --title "Updating package database..." --infobox "\nPlease wait...\n" 11 70
+            sudo apt-get --show-progress -y install dialog git pv bzip2 psmisc libusb-1.0 alsa-utils 2>&1 | grep --line-buffered -oP "(\d+(\.\d+)?(?=%))" | dialog --title "Installing dialog and pv programs if they are not present" --gauge "\nPlease wait...\n" 11 70
+
+# download the retrosmc scripts and files
+
+            wget --no-check-certificate -w 4 -O /home/osmc/RetroPie/scripts/retropie.sh https://raw.githubusercontent.com/mcobit/retrosmc/master/scripts/retropie.sh
+            wget --no-check-certificate -w 4 -O /home/osmc/RetroPie/scripts/retropie_watchdog.sh https://raw.githubusercontent.com/mcobit/retrosmc/master/scripts/retropie_watchdog.sh
+            chmod +x /home/osmc/RetroPie/scripts/retropie.sh
+            chmod +x /home/osmc/RetroPie/scripts/retropie_watchdog.sh
+
+# add fix to config.txt for sound
+
+if [[ ! $(grep "dtparam=audio=on" "/boot/config.txt") ]]; then
+sudo su -c 'echo -e "dtparam=audio=on" >> "/boot/config.txt"'
+fi
+
+# set the output volume
+
+amixer set PCM 100
+
+# clone the retropie git and start the installation
+
+            cd
+            git clone git://github.com/RetroPie/RetroPie-Setup.git
+            cd /home/osmc/RetroPie-Setup
+            sudo ./retropie_setup.sh
+
+# check for the right configuration and existance of the es_input file to ensure joystick autoconfig to work (important on update)
+
+            if [ ! "$(grep Action /home/osmc/.emulationstation/es_input.cfg)" ]; then
+                mkdir "/home/osmc/.emulationstation"
+                cat > "/home/osmc/.emulationstation/es_input.cfg" << _EOF_
+<?xml version="1.0"?>
+<inputList>
+  <inputAction type="onfinish">
+    <command>/opt/retropie/supplementary/emulationstation/scripts/inputconfiguration.sh</command>
+  </inputAction>
+</inputList>
+_EOF_
+            fi
 
 
 # install some programs needed to run the installation 
 
             sudo apt-get update 2>&1 | dialog --title "Updating package database..." --infobox "\nPlease wait...\n" 11 70
             sudo apt-get --show-progress -y install dialog git pv bzip2 psmisc libusb-1.0 alsa-utils pulseaudio 2>&1 | grep --line-buffered -oP "(\d+(\.\d+)?(?=%))" | dialog --title "Installing dialog and pv programs if they are not present" --gauge "\nPlease wait...\n" 11 70
+#download compile and install Attract-Mode
+	    cd ~
+	    mkdir develop
+	    
+#Install "Smfl-pi" And Attract-Mode Dependancies
 
+	   sudo apt-get install cmake libflac-dev libogg-dev libvorbis-dev libopenal-dev libjpeg62-turbo-dev libfreetype6-dev  libudev-dev libavutil-dev libavcodec-dev libavformat-dev libavfilter-dev libswscale-dev libavresample-dev libfontconfig1-dev 
+
+#Download and build sfml-pi
+
+	    cd ~/develop
+	    git clone --depth 1 https://github.com/mickelson/sfml-pi sfml-pi
+	    mkdir sfml-pi/build;cd sfml-pi/build
+	    cmake .. -DSFML_RPI=1 -DEGL_INCLUDE_DIR=/opt/vc/include -DEGL_LIBRARY=/opt/vc/lib/libEGL.so -DGLES_INCLUDE_DIR=/opt/vc/include -DGLES_LIBRARY=/opt/vc/lib/libGLESv1_CM.so
+	    sudo make install
+	    sudo ldconfig
+	    
+#Download and build Attract-Mode
+
+	    cd ~/develop
+	    git clone --depth 1 https://github.com/mickelson/attract attract
+	    cd attract
+	    make USE_GLES=1
+	    sudo make install
+	    
 # download the attract_mode scripts 
 
             wget --no-check-certificate -w 4 -O /home/osmc/RetroPie/scripts/attract.sh https://raw.githubusercontent.com/PiDiaries/Retroattract/blob/master/scripts/attract.sh
@@ -59,15 +130,18 @@ do
 # get the addon archive file from github
 
 	  wget --no-check-certificate -w 4 -O plugin.program.attract-launcher-0.0.1.tar.gz https://raw.githubusercontent.com/PiDiaries/Retroattract/blob/master/plugin.program.attract-launcher-0.0.1.tar.gz 2>&1 | grep --line-buffered -oP "(\d+(\.\d+)?(?=%))" | dialog --title "Downloading Addon" --gauge "\nPlease wait...\n"  11 70
+	  wget --no-check-certificate -w 4 -O plugin.program.retropie-launcher-0.0.1.tgz https://github.com/mcobit/retrosmc/raw/master/plugin.program.retropie-launcher-0.0.1.tgz 2>&1 | grep --line-buffered -oP "(\d+(\.\d+)?(?=%))" | dialog --title "Downloading Addon" --gauge "\nPlease wait...\n"  11 70
 
 # extract the addon to the kodi addon directory
 
 	  (pv -n plugin.program.attract-launcher-0.0.1.tar.gz | sudo tar xzf - -C /home/osmc/ ) 2>&1 | dialog --title "Extracting Addon" --gauge "\nPlease wait...\n" 11 70
 	  dialog --backtitle "Attract-Mode setup script" --title "Installing Addon" --msgbox "\nAddon installed.\n" 11 70
+	  (pv -n plugin.program.retropie-launcher-0.0.1.tgz | sudo tar xzf - -C /home/osmc/ ) 2>&1 | dialog --title "Extracting Addon" --gauge "\nPlease wait...\n" 11 70
+	  dialog --backtitle "RetroPie-OSMC setup script" --title "Installing Addon" --msgbox "\nAddon installed.\n" 11 70
 
 # remove archive file
 
-          rm plugin.program.attract-launcher-0.0.1.tar.gz
+          rm plugin.program.attract-launcher-0.0.1.tar.gz plugin.program.retropie-launcher-0.0.1.tgz
 
 # restart script
 
@@ -77,7 +151,7 @@ do
 
 # delete the addon from kodi addon directory
 
-	   rm -r /home/osmc/.kodi/addons/plugin.program.attract-launcher
+	   rm -r /home/osmc/.kodi/addons/plugin.program.attract-launcher /home/osmc/.kodi/addons/plugin.program.retropie-launcher
 	   dialog --backtitle "Attract-Mode setup script" --title "Removing Addon" --msgbox "\nAddon removed.\n" 11 70
 
 # restart script
@@ -105,36 +179,6 @@ do
 
             exec /home/osmc/install-attract.sh
             ;;
-        5)
-            
-#download compile and install Attract-Mode
-	    cd ~
-	    mkdir develop
-	    
-#Install "Smfl-pi" And Attract-Mode Dependancies
-
-	   sudo apt-get install cmake libflac-dev libogg-dev libvorbis-dev libopenal-dev libjpeg62-turbo-dev libfreetype6-dev  libudev-dev libavutil-dev libavcodec-dev libavformat-dev libavfilter-dev libswscale-dev libavresample-dev libfontconfig1-dev 
-
-#Download and build sfml-pi
-
-	    cd ~/develop
-	    git clone --depth 1 https://github.com/mickelson/sfml-pi sfml-pi
-	    mkdir sfml-pi/build;cd sfml-pi/build
-	    cmake .. -DSFML_RPI=1 -DEGL_INCLUDE_DIR=/opt/vc/include -DEGL_LIBRARY=/opt/vc/lib/libEGL.so -DGLES_INCLUDE_DIR=/opt/vc/include -DGLES_LIBRARY=/opt/vc/lib/libGLESv1_CM.so
-	    sudo make install
-	    sudo ldconfig
-	    
-#Download and build Attract-Mode
-
-	    cd ~/develop
-	    git clone --depth 1 https://github.com/mickelson/attract attract
-	    cd attract
-	    make USE_GLES=1
-	    sudo make install
-# restart script
-
-            exec /home/osmc/install-attract.sh
-            ;;	    
     esac
 done
 Status API Training Shop Blog About
