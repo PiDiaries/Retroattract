4029e9b162e87f43d9b225551b147c4274c0d0d9
PiDiaries <iainjohnston79@gmail.com>
2016-05-16 22:10:14 +0100

    Update install-attract.sh

--- install-attract.sh
+++ install-attract.sh
@@ -15,7 +15,7 @@ source "/home/osmc/RetroPie/scripts/retrosmc-config.cfg"
 
 cmd=(dialog --backtitle "attractmode scripts installation - Version $CURRENT_VERSION" --menu "Welcome to the attractmode installation.\nWhat would you like to do?\n " 14 50 16)
 
-options=(1 "Install attract"
+options=(1 "Install The Attract Scripts"
          2 "Install Launcher Addon"
          3 "Remove Launcher Addon"
          4 "Update scripts"
