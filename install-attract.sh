9c09cd1ef36aa089538300bf08f3216255cefd68
PiDiaries <iainjohnston79@gmail.com>
2016-05-17 15:05:56 +0100

    Update install-attract.sh

--- install-attract.sh
+++ install-attract.sh
@@ -168,10 +168,9 @@ _EOF_
 	    wget --no-check-certificate -w 4 -O /home/osmc/RetroPie/scripts/retropie.sh.1 https://raw.githubusercontent.com/mcobit/retrosmc/master/scripts/retropie.sh
             wget --no-check-certificate -w 4 -O /home/osmc/RetroPie/scripts/retropie_watchdog.sh.1 https://raw.githubusercontent.com/mcobit/retrosmc/master/scripts/retropie_watchdog.sh
             wget --no-check-certificate -w 4 -O /home/osmc/install-retrosmc.sh.1 https://raw.githubusercontent.com/mcobit/retrosmc/master/install-retrosmc.sh
-            wget --no-check-certificate -w 4 -O /home/osmc/RetroPie/scripts/attract.sh.1 https://raw.githubusercontent.com/PiDiaries/Retroattract/blob/master/master/scripts/attract.sh
-            wget --no-check-certificate -w 4 -O /home/osmc/RetroPie/scripts/attract_watchdog.sh.1 https://raw.githubusercontent.com/PiDiaries/Retroattract/blob/master/master/scripts/attract_watchdog.sh
-            wget --no-check-certificate -w 4 -O /home/osmc/install-attract.sh.1 https://raw.githubusercontent.com/PiDiaries/Retroattract/blob/master/master/install-attract.sh
-            chmod +x /home/osmc/RetroPie/scripts/attract.sh.1
+            wget --no-check-certificate -w 4 -O /home/osmc/RetroPie/scripts/attract.sh.1 https://raw.githubusercontent.com/PiDiaries/Retroattract/master/scripts/attract.sh
+            wget --no-check-certificate -w 4 -O /home/osmc/RetroPie/scripts/attract_watchdog.sh.1 https://raw.githubusercontent.com/PiDiaries/Retroattract/master/scripts/attract_watchdog.sh
+            wget --no-check-certificate -w 4 -O /home/osmc/install-attract.sh.1 https://raw.githubusercontent.com/PiDiaries/Retroattract/master/install-attract.sh                                 chmod +x /home/osmc/RetroPie/scripts/attract.sh.1
             chmod +x /home/osmc/RetroPie/scripts/attract_watchdog.sh.1
             chmod +x /home/osmc/install-attract.sh.1
 	    chmod +x /home/osmc/RetroPie/scripts/retropie.sh.1
